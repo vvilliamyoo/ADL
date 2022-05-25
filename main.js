a5862d493ec84d7b6791d4fb86cc3fdf74c7810b
William Yoo <87338673+vvilliamyoo@users.noreply.github.com>
2022-05-25 00:19:01 -0500

    Added website; visual and code template changes

--- main.js
+++ main.js
@@ -19,4 +19,7 @@ function createWindow () {
     win.show()
   })
 }
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+  alert("This is for desktop only, loser <3")
+}
 app.on('ready', createWindow)
